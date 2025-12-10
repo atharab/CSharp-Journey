e1b6e3dd6043a5a280774001ef8733d93f5b20d6
atharab <atharab@gmaile.com>
2025-12-10 04:52:29 -0500

    Module01 Intro: first C# program

---
+++ minor/Module01_Intro/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+Console.WriteLine("Congratulations!");
+Console.Write("You wrote your first lines of code.");

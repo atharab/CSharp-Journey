9d60b6bb740778008c25632d613a855177a6b422
atharab <atharab@gmaile.com>
2025-12-10 15:59:20 -0500

    C# Module01 Unit4

--- minor/Module01_Intro/Program.cs
+++ minor/Module01_Intro/Program.cs
@@ -1,4 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-Console.WriteLine("Congratulations!");
-Console.Write("You wrote your first lines of code.");
+Console.WriteLine("This is the first line");
+Console.Write("This is the second line.");

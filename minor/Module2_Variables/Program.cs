81211bad89e97cd96b1a7b34a78caa913a7169a7
atharab <atharab@gmaile.com>
2025-12-11 15:14:25 -0500

    Module02 CVariables:Unit2

--- minor/Module2_Variables/Program.cs
+++ minor/Module2_Variables/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine('b');
-Console.WriteLine(123);
+// Console.WriteLine(123);
+// Console.WriteLine(0.25F);
+// Console.WriteLine(2.65);
+// Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);

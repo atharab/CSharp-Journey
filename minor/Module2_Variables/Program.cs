3bdb20f192f3a8753d9daf257d385abdd5082726
atharab <atharab@gmaile.com>
2025-12-11 15:25:44 -0500

    Added Module2:Unit3_Variables

--- minor/Module2_Variables/Program.cs
+++ minor/Module2_Variables/Program.cs
@@ -1,8 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//LITERAL VALUES
 // Console.WriteLine('b');
 // Console.WriteLine(123);
 // Console.WriteLine(0.25F);
 // Console.WriteLine(2.65);
 // Console.WriteLine(12.39816m);
-Console.WriteLine(true);
-Console.WriteLine(false);
+//Console.WriteLine(true);
+//Console.WriteLine(false);
+//VARIABLES
+string firstName;
+char userOption;
+int gameScore;
+decimal pariclesPerMillion;
+bool processedCustomer;
+

f35f794b713d5b066ab121a108c7bd583c598f62
atharab <atharab@gmaile.com>
2025-12-12 08:37:28 -0500

    Module3:Unit5

--- minor/Module3_Strings/Program.cs
+++ minor/Module3_Strings/Program.cs
@@ -29,5 +29,12 @@ using System.Net.Security;
 // string message = $"{updateText} {version}";
 // Console.WriteLine($"{updateText} {version}");
 
-string projectName = "First-Project";
-Console.WriteLine($@"C:\Output\{projectName}\Data");
+// string projectName = "First-Project";
+// Console.WriteLine($@"C:\Output\{projectName}\Data");
+
+string projectName = "ACME";
+string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
+string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+Console.WriteLine($"View English output: \n\t{englishLocation}");
+Console.WriteLine($"\n{russianMessage}: \n\t{russianLocation}");

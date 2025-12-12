e8b477aa24764387b6d0171e76791fdb0fd40d0b
atharab <atharab@gmaile.com>
2025-12-12 07:43:17 -0500

    Module3:Unit2

---
+++ minor/Module3_Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Security;
+
+//Console.WriteLine("Hello\nWorld!");
+//Console.WriteLine("Hello\tWorld!");
+//Console.WriteLine("Hello \"World\"!");
+//Console.WriteLine("c:\\source\\repos");
+
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\") ... \n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.Write("\nOutput Directory:\t"); 
+Console.Write(@"c:\invoices");
+
+Console.WriteLine(@"    c:\sources\repos
+       (this is where your code goes)");

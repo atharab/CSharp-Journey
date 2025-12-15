f6a7be1bb23897404d4e11643f2909acedd71639
atharab <atharab@gmaile.com>
2025-12-15 15:10:04 -0500

    Module4_Numbers_unit3

--- minor/Module4_numbers/Program.cs
+++ minor/Module4_numbers/Program.cs
@@ -3,6 +3,35 @@
 // int secondNumber = 7;
 // Console.WriteLine(firstNumber + secondNumber);
 
-string firstName = "Bob";
-int widgetsSold = 7;
-Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+// string firstName = "Bob";
+// int widgetsSold = 7;
+// Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+
+// int sum = 7 + 5;
+// int difference = 7 - 5;
+// int product = 7 * 5;
+// int quotient = 7 / 5;
+
+// Console.WriteLine("Sum: " + sum);
+// Console.WriteLine("difference: " + difference);
+// Console.WriteLine("Product: " + product);
+// Console.WriteLine("Quotient: " + quotient);
+
+// decimal decimalQuotient = 7.0m / 5;
+// Console.WriteLine($"Decimal quotient: {decimalQuotient}");
+
+// int first = 7;
+// int second = 5;
+// decimal quotient = (decimal)first / (decimal)second;
+// Console.WriteLine(quotient);
+
+// Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+// Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+
+// PEMDAS
+
+int value1 = 3 + 4 * 5;
+int value2 = (3 + 4) * 5;
+Console.WriteLine(value1);
+Console.WriteLine(value2);
+

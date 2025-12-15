d664cf6eaf2edad24e2a2477c304ee75fc46ed9f
atharab <atharab@gmaile.com>
2025-12-15 14:43:22 -0500

    Module4_Numbers_unit2

--- minor/Module4_numbers/Program.cs
+++ minor/Module4_numbers/Program.cs
@@ -5,4 +5,4 @@
 
 string firstName = "Bob";
 int widgetsSold = 7;
-Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");

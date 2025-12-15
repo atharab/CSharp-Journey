1ad8ec65d945ad6ebad924fa7215c65593bf8add
atharab <atharab@gmaile.com>
2025-12-15 14:19:48 -0500

    Module4:Numbers_unit2

---
+++ minor/Module4_numbers/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// int firstNumber = 12;
+// int secondNumber = 7;
+// Console.WriteLine(firstNumber + secondNumber);
+
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
